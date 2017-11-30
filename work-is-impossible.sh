9092a0a27c4d264bd7754a4a609de07fb8163eed
HsiangHo <ho_cheung@qq.com>
2017-12-01 02:12:16 +0800

    updated shell

--- work-is-impossible.sh
+++ work-is-impossible.sh
@@ -2,10 +2,19 @@ begin=$(expr $(cat ./begin) + 86400) # init: 1468944000
 end=$(date '+%s')
 
 list=$(seq -f "%.0f" $begin 86400 $end)
+after=''
 
 for i in $list
 do
     days=$((($i - $begin) / 86400 + 1 ))
     date -r $(expr $i) +"%Y年%m月%d日:　　继续打工！　　已打工${days}天  " >> ./README.md
     echo $i > ./begin
+
+    git add ./README.md
+    git add ./begin
+    git commit --date=$i --message='add'
+
+    after='git push origin HEAD'
 done
+
+eval $after
